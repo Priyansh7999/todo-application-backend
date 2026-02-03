5330333c80db68344b63a756c72ac4550d900d9e
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-03 14:28:08 +0530

    refactor:Correct the error return json formate to folloe non functional requirement

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -5,7 +5,10 @@ const createTask = (req, res, next) => {
     const task = taskService.createTask(req.body);
     res.status(201).json(task);
   } catch (err) {
-    next(err);
+    res.status(400).json({"erorr" : {
+      "code": "INVALID_TASK_DATA",
+      "message": err.message,
+    }});
   }
 };
 
@@ -14,7 +17,10 @@ const getAllTasks = (req, res, next) => {
     const tasks = taskService.getTasks(req.query);
     res.json(tasks);
   } catch (err) {
-    next(err);
+    res.status(500).json({"error": {
+      "code": "SERVER_ERROR",
+      "message": "An unexpected error occurred while fetching tasks."
+    }});
   }
 };
 
@@ -23,7 +29,10 @@ const getTaskById = (req, res, next) => {
     const task = taskService.getTaskById(req.params.id);
     res.json(task);
   } catch (err) {
-    next(err);
+    res.status(404).json({ "error": {
+      "code": "TASK_NOT_FOUND",
+      "message": err.message
+    }});
   }
 };
 
@@ -32,7 +41,10 @@ const updateTask = (req, res, next) => {
     const task = taskService.updateTask(req.params.id, req.body);
     res.json(task);
   } catch (err) {
-    next(err);
+    res.status(404).json({ "error": {
+      "code": "TASK_NOT_FOUND",
+      "message": err.message
+    }});
   }
 };
 
@@ -41,7 +53,10 @@ const deleteTask = (req, res, next) => {
     taskService.deleteTask(req.params.id);
     res.status(204).send();
   } catch (err) {
-    next(err);
+    res.status(404).json({ "error": {
+      "code": "TASK_NOT_FOUND",
+      "message": err.message
+    }});
   }
 };
 
