9a4d5ad50282025b5b20a96c85af04c7e0e31e6a
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-04 01:10:42 +0530

    chore:update task validation

--- src/middlewares/validateTask.middleware.js
+++ src/middlewares/validateTask.middleware.js
@@ -4,9 +4,18 @@ const validPriority = ["Low", "Medium", "High"];
 function validateTask(req, res, next) {
   const { title, description, status, priority } = req.body;
 
-  if (!title || title.length > 100) {
+ 
+
+  
+//   if (!title || title.trim().length === 0) {  // Auto-generate title if empty or spaces would love to implement in future
+//     title = description.trim().substring(0, 3);
+//     req.body.title = title;
+//   }
+
+  if ( title.length > 100) {
     return res.status(400).json({ message: "Invalid title (must be 100 characters or less)" });
   }
+
 
   if (!description || description.length > 500) {
     return res.status(400).json({ message: "description should be within 500 words" });
