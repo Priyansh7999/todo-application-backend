425649cfb02c8217bde65b1a03afa1a21d17738c
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-03 12:36:05 +0530

    feat(models): add task domain model

---
+++ models/task.model.js
@@ -0,0 +1,18 @@
+class Task {
+  constructor({ id, title, description, status, priority }) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+    this.priority = priority;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  update(fields) {
+    Object.assign(this, fields);
+    this.updatedAt = new Date();
+  }
+}
+
+module.exports = Task;
