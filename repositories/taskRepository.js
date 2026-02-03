aa5cff3effa8bee8e44a184a7e924975d1994cd9
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-03 13:24:00 +0530

    feat: Check the task by its title

--- repositories/taskRepository.js
+++ repositories/taskRepository.js
@@ -10,8 +10,13 @@ const findTaskById = (id)=>{
 const removeTask = (id) => {
     tasks.delete(id);
 }
+const existsByTitle = (title) =>
+  Array.from(tasks.values())
+    .some(t => t.title.toLowerCase() === title.toLowerCase());
+
 module.exports = {
     saveTask,
     findTaskById,
-    removeTask
+    removeTask,
+    existsByTitle
 }
