26c88558d8cdb3d25f7ef734e040a93b2dfd92c4
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-03 23:27:05 +0530

    feat: add task creation route

---
+++ src/routes/task.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const taskController = require("../controllers/task.controller");
+const validateTask = require("../middlewares/validateTask.middleware");
+
+router.post("/tasks", validateTask, taskController.createTask);
+
+
+
+
+module.exports = router;
