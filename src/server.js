85e30e3b52f53209e1b5214a22be37cdeb5a5d77
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-03 22:54:32 +0530

    chore: set up server configuration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
-const app = require('./app.js');
-const PORT = 3000;
+const app = require("./app");
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+
+
