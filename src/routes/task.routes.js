6f019fca8ae3cb7ea181f887fa913c2a73bf8685
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-04 17:48:48 +0530

    feat(routes): implement GET /v1/tasks/:id endpoint

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,5 +7,5 @@ const validateTask = require("../middlewares/validateTask.middleware");
 router.post("/", validateTask, taskController.createTask);
  router.get("/", taskController.getAllTasks);
 
-
+router.get("/:id", taskController.getSingleTask);
 module.exports = router;
