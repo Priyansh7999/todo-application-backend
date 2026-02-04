51a65014faf1cace62db2b8fb6ad8c6e314bfcad
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-04 12:52:27 +0530

    refactor(services):convert TaskService function to class

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,19 +1,44 @@
+const Task = require("../models/task.model");
 const { generateUUID } = require("../utils/uuid");
-const tasks = [];
 
-function createTask(data) {
-  if (tasks.some(t => t.title === data.title)) 
-    throw new Error("TASK_TITLE_EXISTS");
-  const task = { id: generateUUID(), ...data };
-  tasks.push(task);
-  return task;
+
+class TaskService {
+    constructor() {
+        this.tasks = []; // in memory storage
+    }
+
+    createTask(data) {
+        const isDuplicate = this.tasks.some(
+            task => task.title === data.title
+        );
+
+        if (isDuplicate) {
+            throw new Error("TASK_TITLE_EXISTS");
+        }
+
+        const task = new Task({
+            id: generateUUID(),
+            ...data
+        });
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    getAllTasks(filters = {}) {
+        let result = this.tasks;
+
+        if (filters.status) {
+            result = result.filter(task => task.status === filters.status);
+        }
+
+        if (filters.priority) {
+            result = result.filter(task => task.priority === filters.priority);
+        }
+
+        return result;
+    }
+
 }
 
-function getAllTasks(filters = {}) {
-  let result = tasks;
-  if (filters.status) result = result.filter(t => t.status === filters.status);
-  if (filters.priority) result = result.filter(t => t.priority === filters.priority);
-  return result;
-}
-
-module.exports = { createTask, getAllTasks};
+module.exports = new TaskService();
