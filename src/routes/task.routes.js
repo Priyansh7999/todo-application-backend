99fea84890d46caa5ca597df462049475cd10e5d
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-04 13:39:42 +0530

    feat(routes): add PATCH /:id endpoint for task updates

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,7 +5,8 @@ const taskController = require("../controllers/task.controller");
 const validateTask = require("../middlewares/validateTask.middleware");
 
 router.post("/", validateTask, taskController.createTask);
- router.get("/", taskController.getAllTasks);
+router.get("/", taskController.getAllTasks);
+router.patch("/:id", validateTask, taskController.updateTask);
 
 
 module.exports = router;
