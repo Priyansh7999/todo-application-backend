1338b5e8c8829c83b76d1f57711e61996f6e8bf4
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-05 23:23:55 +0530

    chore:add the JSdoc comments in task routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,13 +1,27 @@
+//src/routes/task.routes.js
 const express = require("express");
 const router = express.Router();
+
+
+/* Task routes  
+* defines the rest API endpoints
+*/
 
 const taskController = require("../controllers/task.controller");
 const validateTask = require("../middlewares/validateTask.middleware");
 
+// create a new task 
 router.post("/", validateTask.validateCreateTask, taskController.createTask);
+
+// get all task with filters
 router.get("/", taskController.getAllTasks);
+
+// update single task
 router.patch("/:id", validateTask.validateUpdateTask, taskController.updateTask);
 
+// get a particulat task
 router.get("/:id", taskController.getSingleTask);
+
+//delete a task
 router.delete("/:id",taskController.deleteTask);
 module.exports = router;
