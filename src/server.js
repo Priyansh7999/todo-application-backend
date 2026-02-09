bd6ceef2a8491b331433c39738d8a1ffcb9b88b3
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-09 13:16:26 +0530

    feat(server): initialize database connection on server startup

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 // src/server.js
 const app = require("./app");
+const pool = require("./config/db");
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database", err);
+    process.exit(1);
+  } else {
+    console.log("Connected to the database");
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-
-
